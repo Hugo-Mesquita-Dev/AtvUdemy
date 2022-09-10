65a845a68d8e63c21e820cc1c7260c1f3ffbe8c6
Hugo Mesquita <hugomesquitadev@gmail.com>
2022-09-09 23:12:51 -0300

    questao 6

--- src/Q2.java
+++ src/Q2.java
@@ -2,6 +2,15 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Q2 {
+
+    /*
+     * Faça um programa para ler o valor do raio de um círculo, e depois mostrar o
+     * valor da área deste círculo com quatro
+     * casas decimais conforme exemplos.
+     * Fórmula da área: area = π . raio2
+     * Considere o valor de π = 3.14159
+     */
+
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
--- src/Q6.java
+++ src/Q6.java
@@ -2,11 +2,23 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Q6 {
+
+    /*
+     * Fazer um programa que leia três valores com ponto flutuante de dupla
+     * precisão: A, B e C. Em seguida, calcule e
+     * mostre:
+     * a) a área do triângulo retângulo que tem A por base e C por altura.
+     * b) a área do círculo de raio C. (pi = 3.14159)
+     * c) a área do trapézio que tem A e B por bases e C por altura.
+     * d) a área do quadrado que tem lado B.
+     * e) a área do retângulo que tem lados A e B
+     */
+
     public static void main(String[] args) throws Exception {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         System.out.println("Considere PI = 3,14");
-        Double pi = 3.145680;
+        Double pi = 3.14;
         System.out.println("Digite o primeiro vaolor: ");
         Double a = sc.nextDouble();
         System.out.println("Difite o segundo valor : ");
